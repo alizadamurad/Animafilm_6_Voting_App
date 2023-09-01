73c947d2421cdf1eb81ffc240c25b26634a9edad
alizadamurad <elizademurad03@gmail.com>
2023-09-01 19:41:33 +0400

    CVS export logic changed

--- lib/Controllers/cvs_file_converter.dart
+++ lib/Controllers/cvs_file_converter.dart
@@ -28,7 +28,7 @@ class CvsController extends GetxController {
 
     List<List<dynamic>> rows = [];
     List<dynamic> empty = [];
-
+    // Best Short Grand Prix A
     List<dynamic> rowbestShortA = [];
     rowbestShortA.add('Best Short Animated Film - GRAND PRIX - A');
     rowbestShortA.add('Votes');
@@ -41,6 +41,7 @@ class CvsController extends GetxController {
       row.add(film.value.value);
       rows.add(row);
     }
+    //Best Short Grand Prix B
     List<dynamic> rowbestShortB = [];
     rows.add(empty);
     rowbestShortB.add('Best Short Animated Film - GRAND PRIX - B');
@@ -49,6 +50,50 @@ class CvsController extends GetxController {
     filmController.bestShortAnimatedB
         .sort((a, b) => a.title.compareTo(b.title));
     for (var film in filmController.bestShortAnimatedB) {
+      List<dynamic> row = [];
+      row.add(film.title);
+      row.add(film.value.value);
+      rows.add(row);
+    }
+
+    //Best Short For Children - A
+    List<dynamic> rowbestShortAnimatedChildA = [];
+    rows.add(empty);
+    rowbestShortAnimatedChildA.add('Best Short Animated Film for Children - A');
+    rowbestShortAnimatedChildA.add('Votes');
+    rows.add(rowbestShortAnimatedChildA);
+    filmController.bestShortAnimatedForChildrenA
+        .sort((a, b) => a.title.compareTo(b.title));
+    for (var film in filmController.bestShortAnimatedForChildrenA) {
+      List<dynamic> row = [];
+      row.add(film.title);
+      row.add(film.value.value);
+      rows.add(row);
+    }
+    //Best Short For Children - B
+    List<dynamic> rowbestShortAnimatedChildB = [];
+    rows.add(empty);
+    rowbestShortAnimatedChildB.add('Best Short Animated Film for Children - B');
+    rowbestShortAnimatedChildB.add('Votes');
+    rows.add(rowbestShortAnimatedChildB);
+    filmController.bestShortAnimatedForChildrenB
+        .sort((a, b) => a.title.compareTo(b.title));
+    for (var film in filmController.bestShortAnimatedForChildrenB) {
+      List<dynamic> row = [];
+      row.add(film.title);
+      row.add(film.value.value);
+      rows.add(row);
+    }
+
+    //Best Short Azerbaijan
+    List<dynamic> rowbestShortAzerbaijan = [];
+    rows.add(empty);
+    rowbestShortAzerbaijan.add('Best Short Azerbaijani Animated Film');
+    rowbestShortAzerbaijan.add('Votes');
+    rows.add(rowbestShortAzerbaijan);
+    filmController.bestShortAzerbaijanAnimated
+        .sort((a, b) => a.title.compareTo(b.title));
+    for (var film in filmController.bestShortAzerbaijanAnimated) {
       List<dynamic> row = [];
       row.add(film.title);
       row.add(film.value.value);
